d18076d1718b730d67272d316ecec6ad24eb5d0c
Saito <saitowu@gmail.com>
2013-11-02 13:29:38 +0800

    works with precompile, Yay!

--- lib/handlebars.rb
+++ lib/handlebars.rb
@@ -1,5 +1,35 @@
-require "handlebars/version"
+require 'execjs'
+require 'handlebars/source'
+require 'handlebars/version'
 
 module Handlebars
-  # Your code goes here...
+  module Source
+    def self.path
+      @path ||= bundled_path
+    end
+
+    def self.contents
+      @contents ||= File.read(path)
+    end
+
+    def self.version
+      # Handlebars.VERSION = "1.0.0";
+      @version ||= contents[/Handlebars\.VERSION\s=\s\"([\d.]+)\"/, 1]
+    end
+
+    def self.context
+      @context ||= ExecJS.compile(contents)
+    end
+  end
+
+  class << self
+    def version
+      Source.version
+    end
+
+    def precompile(script)
+      script = script.read if script.respond_to?(:read)
+      Source.context.call("Handlebars.precompile", script)
+    end
+  end
 end
