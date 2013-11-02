4313b2177f51883d6ec6d30165e4bdba8a185200
Saito <saitowu@gmail.com>
2013-11-02 14:38:24 +0800

    should detect error when precompile handlebars template

--- spec/handlebars/handlebars.rb
+++ spec/handlebars/handlebars.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 
 describe Handlebars do
   it "should has version" do
-    Handlebars.version.should == 1.0.0
+    Handlebars.version.should == "1.0.0"
   end
 
   it "should enable to precompile handlebars template" do
-    begin
-      Handlebars.precompile "<p>Hello World</p>"
-      Handlebars.precompile "<p>{{name}}</p>"
-      Handlebars.precompile "{{#each people}}{{name}}{{/each}}"
-      Handlebars.precompile "{{#each people}}{{> person}}{{/people}}"
-    rescue
-      true.should be_false
-    end
+    Handlebars.precompile "<p>Hello World</p>"
+    Handlebars.precompile "<p>{{name}}</p>"
+    Handlebars.precompile "{{#each people}}{{name}}{{/each}}"
+    Handlebars.precompile "{{#each people}}{{> person}}{{/each}}"
+  end
+
+  it "should raise errors when precompile failed" do
+    Handlebars.precompile "{{#each people}}{{name}}{{/if}}" rescue true
+    Handlebars.precompile "{{#each people}}{{< name}}{{/each}}" rescue true
   end
 end
